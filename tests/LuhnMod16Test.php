3225b5e3afb878c82b33d0e2a9326f1bcf73187c
samlittler <prinsig.webmaster@gmail.com>
2016-11-17 14:09:45 +0000

    Fix test

--- tests/LuhnMod16Test.php
+++ tests/LuhnMod16Test.php
@@ -5,7 +5,7 @@ class LuhnModNTest extends \PHPUnit\Framework\TestCase
     public function testCalculatesCorrectly()
     {
         $test_value = '0B012722900021AC35B2';
-        $checksum = \LuhnMod16\LuhnMod16::calculate($test_value, 16);
+        $checksum = \LuhnMod16\LuhnMod16::calculate($test_value);
         $this->assertEquals($checksum, '1');
     }
 }
