895638125a74e63111e201821895db1fedfb7c13
samlittler <prinsig.webmaster@gmail.com>
2016-11-17 14:41:19 +0000

    A polite notice

--- tests/LuhnMod16Test.php
+++ tests/LuhnMod16Test.php
@@ -2,6 +2,11 @@
 
 class LuhnModNTest extends \PHPUnit\Framework\TestCase
 {
+    /*
+	 * The nature of this checksum means that it's wrong 1/16 times. What kind of
+	 * checksum is that, huh? Please include new tests if you get a chance, so
+     * that we can continue to prove the effectiveness of this library.
+	 */
     public function testCalculatesCorrectly()
     {
         $test_value = '0B012722900021AC35B2';
