028beba603b80bc9f9016450e29eceda7d3d2010
samlittler <prinsig.webmaster@gmail.com>
2016-11-17 14:43:19 +0000

    Change notice

--- tests/LuhnMod16Test.php
+++ tests/LuhnMod16Test.php
@@ -3,10 +3,10 @@
 class LuhnModNTest extends \PHPUnit\Framework\TestCase
 {
     /*
-     * The nature of this checksum means that it's wrong 1/16 times. What kind of
-     * checksum is that, huh? Please include new tests if you get a chance, so
-     * that we can continue to prove the effectiveness of this library.
-    */
+     * The nature of this checksum means that 1/16 times it'll be correct anyway.
+     * What kind of checksum is that, huh? Please provide more tests if you get the
+     * chance so that we can continue to prove the effectiveness of the this library.
+     */
     public function testCalculatesCorrectly()
     {
         $test_value = '0B012722900021AC35B2';
